3e8490ff40174ac940acf02dcfe7ef68811938df
jean-hugues DOUMBE K <jean-hugues-externe.doumbe-kingue@edf.fr>
2022-05-17 10:58:08 +0200

    Build local and production dockerfile

---
+++ README.md
@@ -0,0 +1,61 @@
+# Dojo App
+
+> Dojo App is a project management website built with React and Firebase. It allows users to collaborate on projects, chat with each other 
+
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Features](#features)
+- [Status](#status)
+- [Available Scripts](#available-scripts)
+- [Inspiration](#inspiration)
+- [Contact](#contact)
+
+## General info
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+The project is a part of my React learning path from the ground up to make dynamic websites. I use custom react hooks to interact with different firebase services
+
+## Technologies
+
+- React 18.0.0
+- React-Router-Dom 6.3.0
+- Firebase 8.5
+
+
+## Features
+
+List of features:
+
+- Firebase Authentication
+- Firebase Storage
+- Custom React Hooks
+- Image uploads
+- List online users
+
+
+## Status
+
+Project is: _in progress_
+
+## Available Scripts
+
+# create and run container locally
+docker-compose -f docker-compose-local.yml up -d
+
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+In the project directory, you can run:
+
+# create and run container locally
+docker-compose up -d
+
+
+## Inspiration
+
+Project based on Build Web Apps with React & Firebase by The Net Ninja 
+
+## Contact
+
+Created by [@hugonin](https://github.com/hugonin) - feel free to contact me!
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import OnlineUsers from "./components/onlineUsers/OnlineUsers";
+
+import "./App.css";
+
+
+function App() {
+  const { authIsReady, user } = useAuthContext();
+  return (
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/create"
+                element={user ? <Create /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/projects/:id"
+                element={user ? <Project /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+              />
+              <Route
+                path="/signup"
+                element={user ? <Navigate to="/" /> : <Signup />}
+              />
+            </Routes>
+          </div>
+          { user && <OnlineUsers />}
+        </BrowserRouter>
+      )}
+    </div>
+  );
+}
+
+export default App;
